e58b87f159347bd9162c5aa343f0dfc388adedec
pmarty <p-marty@sunrise.ch>
2015-10-29 23:13:21 +0100

    ADD: Word Class not finished

--- Word/src/Word.h
+++ Word/src/Word.h
@@ -8,6 +8,51 @@
 #ifndef WORD_H_
 #define WORD_H_
 
+#include <iostream>
+
+class Word {
+
+	static std::string word{};
+	explicit Word(std::istream& in);
+
+public:
+
+	std::istream & read(std::istream& in);
+	std::ostream & print(std::ostream& out) const;
+
+	bool operator<(Word const& rhs) const;
+
+	inline std::ostream& operator<<(std::ostream& out, Word const& w) {
+		w.print(out);
+		return out;
+	}
+
+	inline std::istream& operator>>(std::istream& in, Word& w) {
+		w.read(in);
+		return in;
+	}
+};
+
+
+inline bool operator>(Word const& lhs, Word const& rhs) const {
+	return rhs < lhs;
+}
+
+inline bool operator>=(Word const& lhs, Word const& rhs) const {
+	return !(lhs < rhs);
+}
+
+inline bool operator<=(Word const& lhs, Word const& rhs) const {
+	return !(rhs < lhs);
+}
+
+inline bool operator==(Word const& lhs, Word const& rhs) const {
+	return !(lhs < rhs) && !(rhs < lhs);
+}
+
+inline bool operator!=(Word const& lhs, Word const& rhs) const {
+	return !(lhs == rhs);
+}
 
 
 
