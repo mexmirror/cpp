1f66599394c26769ddeae89fd90697de13b3cad5
pmarty <p-marty@sunrise.ch>
2015-10-29 23:13:21 +0100

    ADD: Word Class not finished

--- Word/src/Word.cpp
+++ Word/src/Word.cpp
@@ -5,6 +5,26 @@
  *      Author: hsr
  */
 
+#include "Word.h"
 
+Word::Word(std::istream& in) {
+	read(in);
+	if(in.fail()) throw std::out_of_range{"invalid word"};
+};
+
+std::ostream & Word::print(std::ostream& out) const {
+	out << word;
+	return out;
+}
+
+std::istream & Word::read(std::istream& in) {
+	if(in.eof()) {
+		in.setstate(std::ios::failbit | in.rdstate());
+	} if(in.good()) {
+		// TODO
+	}
+
+	return in;
+}
 
 
