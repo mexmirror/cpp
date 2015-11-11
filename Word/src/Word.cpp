d62a710897131feb222c79be5b895820b76fc785
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-11-11 14:09:17 +0100

    MOD: read function

--- Word/src/Word.cpp
+++ Word/src/Word.cpp
@@ -22,19 +22,19 @@ std::istream & Word::read(std::istream& in) {
 	if(in.eof()) {
 		in.setstate(std::ios::failbit | in.rdstate());
 	} if(in.good()) {
+		bool firstChar = true;
 		std::string temp {};
 		char c {};
 		in.get(c);
 		while(in.peek() != EOF){
-			int size = in.gcount();
 			if(std::isalpha(c)){
 				temp += c;
 				in.get(c);
-			} else if(temp.size() == 0){
-				in.get();
+				firstChar = false;
+			} else if(firstChar){
+				in >> c;
 			} else {
 				currentContent = temp;
-
 				return in;
 			}
 		}
