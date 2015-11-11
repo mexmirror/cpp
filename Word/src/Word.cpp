02c82ed0cd2ca0719f881352354970cf33735590
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-11-11 13:00:58 +0100

    MOD: word class

--- Word/src/Word.cpp
+++ Word/src/Word.cpp
@@ -1,19 +1,11 @@
-/*
- * Word.cpp
- *
- *  Created on: 28.10.2015
- *      Author: hsr
- */
-
 #include "Word.h"
+#include <cctype>
 
-Word::Word(std::istream& in) {
-	read(in);
-	if(in.fail()) throw std::out_of_range{"invalid word"};
-};
-
+Word::Word() {
+	std::string currentContent {};
+}
 std::ostream & Word::print(std::ostream& out) const {
-	out << word;
+	out << currentContent;
 	return out;
 }
 
@@ -21,10 +13,23 @@ std::istream & Word::read(std::istream& in) {
 	if(in.eof()) {
 		in.setstate(std::ios::failbit | in.rdstate());
 	} if(in.good()) {
-		// TODO
-	}
+		std::string temp {};
+		char c {};
+		in.get(c);
+		while(in.peek() != EOF){
+			int size = in.gcount();
+			if(std::isalpha(c)){
+				temp += c;
+				in.get(c);
+			} else if(temp.size() == 0){
+				in.get();
+			} else {
+				currentContent = temp;
 
+				return in;
+			}
+		}
+		currentContent = temp;
+	}
 	return in;
 }
-
-
--- Word/src/Word.h
+++ Word/src/Word.h
@@ -1,60 +1,57 @@
-/*
- * Word.h
- *
- *  Created on: 28.10.2015
- *      Author: hsr
- */
-
 #ifndef WORD_H_
 #define WORD_H_
 
 #include <iostream>
+#include <cctype>
+#include<iterator>
+#include <algorithm>
 
 class Word {
 
-	static std::string word{};
-	explicit Word(std::istream& in);
+	std::string currentContent{};
 
 public:
-
+	Word();
 	std::istream & read(std::istream& in);
 	std::ostream & print(std::ostream& out) const;
 
-	bool operator<(Word const& rhs) const;
+	inline bool operator<(Word const& rhs) const{
+		std::string s1 {}, s2 {};
+		std::transform(begin(this->currentContent), end(this->currentContent), begin(s1), ::tolower);
+		std::transform(begin(rhs.currentContent), end(rhs.currentContent),begin(s2), ::tolower);
+		return s1 < s2;
+	}
 
-	inline std::ostream& operator<<(std::ostream& out, Word const& w) {
-		w.print(out);
+	inline std::ostream& operator<<(std::ostream& out) {
+		this->print(out);
 		return out;
 	}
 
-	inline std::istream& operator>>(std::istream& in, Word& w) {
-		w.read(in);
+	inline std::istream& operator>>(std::istream& in) {
+		this->read(in);
 		return in;
 	}
 };
 
 
-inline bool operator>(Word const& lhs, Word const& rhs) const {
+inline bool operator >(Word const& lhs, Word const& rhs) {
 	return rhs < lhs;
 }
 
-inline bool operator>=(Word const& lhs, Word const& rhs) const {
+inline bool operator >=(Word const& lhs, Word const& rhs) {
 	return !(lhs < rhs);
 }
 
-inline bool operator<=(Word const& lhs, Word const& rhs) const {
+inline bool operator <=(Word const& lhs, Word const& rhs) {
 	return !(rhs < lhs);
 }
 
-inline bool operator==(Word const& lhs, Word const& rhs) const {
+inline bool operator ==(Word const& lhs, Word const& rhs) {
 	return !(lhs < rhs) && !(rhs < lhs);
 }
 
-inline bool operator!=(Word const& lhs, Word const& rhs) const {
+inline bool operator !=(Word const& lhs, Word const& rhs) {
 	return !(lhs == rhs);
 }
 
-
-
-
-#endif /* WORD_H_ */
+#endif
