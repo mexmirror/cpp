86aab6b511c30a09fe087c0a12f578710aa1fbc1
pmarty <p-marty@sunrise.ch>
2015-12-02 16:14:58 +0100

    MOD: indexable set header

--- IndexableSet/src/IndexableSet.h
+++ IndexableSet/src/IndexableSet.h
@@ -1,6 +1,44 @@
 #ifndef INDEXABLESET_H_
 #define INDEXABLESET_H_
 
+#include <set>
+#include <stdexcept>
+#include <cmath>
+
+template<typename T, typename COMPARE=std::less<T>>
+class indexableSet
+:public std::set<T,COMPARE>{
+	using Base=std::set<T,COMPARE>;
+
+public:
+	using size_type=typename Base::size_type;
+	using Base::set;
+
+	T const & at(size_type index) const {
+		auto it = this->begin();
+		std::advance(it, index);
+		return *it;
+	}
+
+	T const & operator[](size_type index) const {
+		if(this->size() < std::abs(index)) {
+			throw std::out_of_range("Out of range index");
+		}
+		if(index < 0) {
+			return this->at(this->size() + index);
+		}
+		return this->at(index);
+	}
+
+	const T & front() const {
+		return this->at(0);
+	}
+
+	const T & back() const {
+		return this->at(-1);
+	}
+
+};
 
 
 
